6ac2d5aa89d6e7f0b9ec74eb06acbc569cdd174b
Alan Bang <alan.bang@outlook.com>
2024-12-05 22:54:25 +0800

    Deploy website - based on 56d2fca787f61ae50196897dc785be7b2f3e055b

---
+++ assets/js/ef8b811a.aff4b15b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkganfan_tech_website=self.webpackChunkganfan_tech_website||[]).push([[8947],{6600:a=>{a.exports=JSON.parse('{"authors":[{"name":"Alan Bang","title":"Full Stack Engineer @ Ganfan Tech","url":"https://github.com/abangfundi","imageURL":"https://github.com/abangfundi.png","key":"abangfundi","page":null,"count":3}]}')}}]);
